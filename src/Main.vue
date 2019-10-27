e937643b6f7633ff29844468e05e08b90320b985
Jiajie Chen <noc@jiegec.ac.cn>
2019-10-27 17:06:48 +0800

    Visualize all legs and fields

--- src/Main.vue
+++ src/Main.vue
@@ -2,9 +2,24 @@
   <div>
     <h1>Input BCBP here</h1>
     <textarea v-model="content"></textarea>
-    <h3>Result</h3>
-    <p v-if="result.passengerName">Name: {{ result.passengerName }}</p>
-    <p v-if="result.passengerDescription">Description: {{ passengerDescription }}</p>
+    <h2>Result</h2>
+    <p v-for="field in fields"> {{ field }} </p>
+    <div v-for="entry, i in entries">
+      <p>Entry #{{ i+1 }}</p>
+      <table>
+        <thead>
+          <th>Key</th>
+          <th>Value</th>
+        </thead>
+        <tbody>
+          <tr v-for="v in entry">
+            <td>{{ v[0] }}</td>
+            <td>{{ v[1] }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <table></table>
     <h3>Original Result from georgesmith46/bcbp</h3>
     {{ result }}
   </div>
@@ -12,19 +27,38 @@
 
 <script>
 import * as bcbp from "bcbp";
+import lodash from "lodash";
 export default {
   name: "Main",
   data: () => ({
-    content: "M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 226F001A0025100", // Taken from BCBP Standard (Resolution 792) example
+    content: "M1DESMARAIS/LUC       EABC123 YULFRAAC 0834 226F001A0025100" // Taken from BCBP Standard (Resolution 792) example
   }),
   computed: {
-    passengerDescription() {
-      return "";
+    fields() {
+        let result = [];
+        for (let key in this.result) {
+          let value = this.result[key];
+          if (key == "legs") {
+              continue;
+          }
+          result.push(lodash.startCase(key) + ": " + value);
+        }
+        return result;
     },
     result() {
       return bcbp.decode(this.content);
+    },
+    entries() {
+      return this.result.legs.map(leg => {
+        let result = [];
+        for (let key in leg) {
+          let value = leg[key];
+          result.push([lodash.startCase(key), value]);
+        }
+        return result;
+      });
     }
-  },
+  }
 };
 </script>
 
